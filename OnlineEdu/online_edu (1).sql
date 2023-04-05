cc356594ad021c13688f0495fdd54619d7c34aa8
Vidhi <20se02ce054@ppsu.ac.in>
2023-04-05 09:30:44 +0530

    database updated

--- OnlineEdu/online_edu (1).sql
+++ OnlineEdu/online_edu (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 03, 2023 at 06:44 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Host: 127.0.0.1
+-- Generation Time: Apr 05, 2023 at 05:46 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `admin` (
   `email` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admin`
@@ -40,6 +40,28 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `email`, `username`, `pass`) VALUES
 (1, 'admin@gmail.com', 'admin123', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`id`, `name`, `email`, `message`, `created_date`) VALUES
+(1, 'shreya', 'shreya@yahoo.com', 'join', '2023-03-25'),
+(3, 'krisha', 'krisha@gmail.com', 'i want to join', '2023-04-04');
 
 -- --------------------------------------------------------
 
@@ -58,7 +80,7 @@ CREATE TABLE `course_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` date NOT NULL DEFAULT current_timestamp(),
   `date_updated` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `course_list`
@@ -87,7 +109,7 @@ CREATE TABLE `course_request` (
   `student_userName` varchar(255) NOT NULL,
   `status` int(4) NOT NULL,
   `delete_flag` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `course_request`
@@ -108,28 +130,28 @@ INSERT INTO `course_request` (`id`, `tutor_id`, `course_id`, `course_name`, `stu
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inquiry_list`
+-- Table structure for table `inquiries`
 --
 
-CREATE TABLE `inquiry_list` (
-  `id` int(30) NOT NULL,
-  `tutor_id` int(30) NOT NULL,
-  `course_id` int(30) NOT NULL,
-  `fullname` text NOT NULL,
-  `email` text NOT NULL,
-  `contact` text NOT NULL,
-  `message` text NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `inquiries` (
+  `id` int(11) NOT NULL,
+  `student_id` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(255) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `delete_flag` int(11) NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `inquiry_list`
+-- Dumping data for table `inquiries`
 --
 
-INSERT INTO `inquiry_list` (`id`, `tutor_id`, `course_id`, `fullname`, `email`, `contact`, `message`, `status`, `date_created`, `date_updated`) VALUES
-(1, 1, 1, 'Michael Williams', 'mcooper@gmail.com', '09123456789', 'Mauris sit amet facilisis mi. Suspendisse potenti. Cras a accumsan lacus, ut vehicula felis. Aliquam suscipit, odio at varius euismod, lorem erat commodo urna, non consectetur justo lectus nec nunc. In ullamcorper, dui quis convallis volutpat, metus nulla rutrum ex, sed tempus nisl turpis id odio. Proin sit amet tincidunt tellus. In hac habitasse platea dictumst. In vitae nisl in felis consequat interdum. Ut semper velit sed magna placerat molestie. Maecenas varius posuere elit, vitae gravida nunc auctor sed. \r\n\r\nDonec molestie turpis vel massa malesuada, a fermentum diam aliquam. Aenean dolor leo, elementum a vulputate et, cursus eget ligula. Maecenas varius accumsan nisl viverra commodo. Etiam ac neque consectetur, placerat neque non, feugiat tellus. Proin eu auctor mauris. Aliquam gravida elit a velit ultrices hendrerit a eget tortor.', 0, '2022-05-17 16:11:49', '2022-05-17 16:11:49');
+INSERT INTO `inquiries` (`id`, `student_id`, `username`, `email`, `course_id`, `course_name`, `tutor_id`, `message`, `delete_flag`, `date_created`) VALUES
+(1, '', 'vidhi', 'vidhi54@gmail.com', 0, 'php', 0, '123', 0, '2023-04-04');
 
 -- --------------------------------------------------------
 
@@ -148,7 +170,7 @@ CREATE TABLE `student` (
   `filename` varchar(100) NOT NULL,
   `qualify` varchar(255) NOT NULL,
   `dob` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `student`
@@ -157,7 +179,7 @@ CREATE TABLE `student` (
 INSERT INTO `student` (`id`, `fullname`, `username`, `email`, `phonenumber`, `pass`, `gender`, `filename`, `qualify`, `dob`) VALUES
 (1, 'Maulik Vastarpara', 'maulik7332', 'maulik@gmail.com', '9737277332', '123', 'Male', '', '', '2023-03-23'),
 (2, 'Vrujal Laheri', 'vrujal30', 'vrujal30@gmail.com', '9074628476', '123', 'Female', '', '', '2023-03-23'),
-(3, 'Vidhi Thummar', 'vidhi54', 'vidhi54@gmail.com', '9737465789', '123', 'Female', 'f1.jpg', 'Btech', '2023-03-10');
+(3, 'Vidhi Thummar', 'vidhi54', 'vidhi54@gmail.com', '9737465789', '123', 'Female', 'girl.jpeg', 'Btech', '2023-03-10');
 
 -- --------------------------------------------------------
 
@@ -177,7 +199,7 @@ CREATE TABLE `tutor_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` date NOT NULL,
   `date_updated` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tutor_list`
@@ -186,7 +208,7 @@ CREATE TABLE `tutor_list` (
 INSERT INTO `tutor_list` (`id`, `firstname`, `middlename`, `lastname`, `email`, `password`, `avtar`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
 (1, 'Mark', 'D', 'Cooper', 'mcooper@gmail.com', 'c7162ff89c647f444fcaa5c635dac8c3', '1.png', 2, 0, '2022-05-17', '2023-03-23'),
 (2, 'Claire', 'C', 'Blake', 'cblake@sample.com', '4744ddea876b11dcb1d169fadf494418', '2.png', 1, 0, '2022-05-17', '2023-03-23'),
-(3, 'Samantha Jane', 'C', 'Miller', 'sam23@gmail.com', 'b60367cae35de6594b1a09bf44a3a68b', '3.jpeg', 0, 0, '2022-05-18', '2023-03-23');
+(3, 'Samantha Jane', 'C', 'Miller', 'sam23@gmail.com', 'b60367cae35de6594b1a09bf44a3a68b', '3.png', 0, 0, '2022-05-18', '2023-03-23');
 
 -- --------------------------------------------------------
 
@@ -195,10 +217,24 @@ INSERT INTO `tutor_list` (`id`, `firstname`, `middlename`, `lastname`, `email`, 
 --
 
 CREATE TABLE `tutor_meta` (
-  `tutor_id` int(20) NOT NULL,
-  `meta_field` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `meta_value` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `meta_id` int(40) NOT NULL,
+  `tutor_id` int(40) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `speciality` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tutor_meta`
+--
+
+INSERT INTO `tutor_meta` (`meta_id`, `tutor_id`, `dob`, `gender`, `contact`, `address`, `speciality`, `description`) VALUES
+(1, 1, '1997-06-23', 'Male', '09123456789', '518 Evangelista, Manila, Metro Manila', 'HTML, CSS, JS, Python, PHP, MYSQL, SQLite, AngularJS, and Node.JS', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean blandit leo vel quam ultricies ultrices. Nam sit amet arcu diam. Cras in augue tempor, imperdiet ligula scelerisque, aliquet dolor. Nulla interdum mi at justo condimentum, ac euismod tellus s'),
+(2, 2, '1997-06-23', 'Female', '09789654123', '26,shukan bunglows b/h setu residency,surat', 'PHP, HTML, CSS, JS, and Python.', 'This is a sample description about myself.\r\n'),
+(3, 3, '1997-10-14', 'female', '09654789123 / 098785466', ' 469 Gen. Luna St., Hulong Duhat, Malabon, Metro Manila', 'Grammar, English, Science, and Mathematics', 'Integer a mi quam. Vivamus et purus sed velit laoreet maximus. Suspendisse erat metus, efficitur sit amet blandit a, imperdiet sed sapien. Praesent lacinia, metus vitae mollis pharetra, enim ex laoreet erat, sit amet egestas nisi diam quis nisi. Praesent ');
 
 --
 -- Indexes for dumped tables
@@ -213,6 +249,12 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `username` (`username`);
 
 --
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `course_list`
 --
 ALTER TABLE `course_list`
@@ -222,6 +264,12 @@ ALTER TABLE `course_list`
 -- Indexes for table `course_request`
 --
 ALTER TABLE `course_request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inquiries`
+--
+ALTER TABLE `inquiries`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -242,7 +290,7 @@ ALTER TABLE `tutor_list`
 -- Indexes for table `tutor_meta`
 --
 ALTER TABLE `tutor_meta`
-  ADD KEY `tutor_id` (`tutor_id`);
+  ADD UNIQUE KEY `meta_id` (`meta_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -253,6 +301,12 @@ ALTER TABLE `tutor_meta`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `course_list`
@@ -267,10 +321,16 @@ ALTER TABLE `course_request`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
+-- AUTO_INCREMENT for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tutor_list`
--- OnlineEdu/student/SendInquiries.php
+++ OnlineEdu/student/SendInquiries.php
@@ -4,12 +4,6 @@ extract($_POST);
 $sql = "INSERT into inquiries (student_id,username,email,course_name,message,date_created) VALUES('" . $id . "','" . $name . "','" . $email . "','" . $coursename . "','" . $message . "','" . date('Y-m-d') . "')";
 $success = $conn->query($sql);
 
-// $sql2 = "SELECT id FROM course_list where name ='" . $coursename . "'";
-
-// $result = $conn->query($sql2);
-
-
-// $sql3 = "SELECT tutor_id FROM course_list where name ='" . $coursename . "'";
 
 if (!$success) {
     die("Couldn't enter data: " . $conn->error);
